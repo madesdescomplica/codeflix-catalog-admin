e095228127c0aca1c88300cdd75d509fbea4f732
mades eladrian <mades.eladrian@gmail.com>
2024-06-15 19:14:32 -0300

    ensure CategoryRepository raises a NotImplementedError if list method is not implemented

--- src/core/category/tests/domain/test_category_repository.py
+++ src/core/category/tests/domain/test_category_repository.py
@@ -1,5 +1,6 @@
 from inspect import isabstract
 from unittest.mock import patch
+from uuid import uuid4
 
 from faker import Faker
 import pytest
@@ -9,40 +10,60 @@ from src.core.category.domain import Category, CategoryRepository
 
 class TestCategoryRepository:
     faker = Faker()
-    name=faker.word(),
-    description=faker.sentence()
+
+    @pytest.fixture
+    def category(self) -> Category:
+        return Category(
+            id=uuid4(),
+            name=self.faker.word(),
+            description=self.faker.sentence(),
+            is_active=True
+        )
+
+    @pytest.fixture
+    @patch.multiple(CategoryRepository, __abstractmethods__=set())
+    def category_repository(self) -> CategoryRepository:
+        return CategoryRepository()
+
 
     def test_should_CategoryRepository_is_an_abstract_class(self):
         assert isabstract(CategoryRepository)
 
-    @patch.multiple(CategoryRepository, __abstractmethods__=set())
-    def test_should_CategoryRepository_raise_a_NotImplementedError_if_save_method_is_not_implemented(self):
-        category = Category(name=self.name, description=self.description)
-        category_repository = CategoryRepository()
-
+    def test_should_CategoryRepository_raise_a_NotImplementedError_if_save_method_is_not_implemented(
+        self,
+        category_repository: CategoryRepository,
+        category: Category
+    ):
         with pytest.raises(NotImplementedError, match='Should implement method: save'):
             category_repository.save(category)
 
-    @patch.multiple(CategoryRepository, __abstractmethods__=set())
-    def test_should_CategoryRepository_raise_a_NotImplementedError_if_get_by_id_method_is_not_implemented(self):
-        category = Category(name=self.name, description=self.description)
-        category_repository = CategoryRepository()
-
+    def test_should_CategoryRepository_raise_a_NotImplementedError_if_get_by_id_method_is_not_implemented(
+        self,
+        category_repository: CategoryRepository,
+        category: Category
+    ):
         with pytest.raises(NotImplementedError, match='Should implement method: get_by_id'):
             category_repository.get_by_id(category.id)
 
-    @patch.multiple(CategoryRepository, __abstractmethods__=set())
-    def test_should_CategoryRepository_raise_a_NotImplementedError_if_update_method_is_not_implemented(self):
-        category = Category(name=self.name, description=self.description)
-        category_repository = CategoryRepository()
-
+    def test_should_CategoryRepository_raise_a_NotImplementedError_if_update_method_is_not_implemented(
+        self,
+        category_repository: CategoryRepository,
+        category: Category
+    ):
         with pytest.raises(NotImplementedError, match='Should implement method: update'):
             category_repository.update(category)
 
-    @patch.multiple(CategoryRepository, __abstractmethods__=set())
-    def test_should_CategoryRepository_raise_a_NotImplementedError_if_delete_method_is_not_implemented(self):
-        category = Category(name=self.name, description=self.description)
-        category_repository = CategoryRepository()
-
+    def test_should_CategoryRepository_raise_a_NotImplementedError_if_delete_method_is_not_implemented(
+        self,
+        category_repository: CategoryRepository,
+        category: Category
+    ):
         with pytest.raises(NotImplementedError, match='Should implement method: delete'):
             category_repository.delete(category.id)
+
+    def test_should_CategoryRepository_raise_a_NotImplementedError_if_list_method_is_not_implemented(
+        self,
+        category_repository: CategoryRepository
+    ):
+        with pytest.raises(NotImplementedError, match='Should implement method: list'):
+            category_repository.list()
